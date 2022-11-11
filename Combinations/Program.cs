11405931c8061de242b80ceb08067c8e26a74a42
jd_786 <jaydeep_shil@certree.com>
2022-11-11 11:31:41 +0530

    Add project files.

---
+++ Combinations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Combinations
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Solution s = new Solution();
+      int n = 5; int k = 3;
+      var answer = s.Combine(n, k);
+      foreach (var a in answer) Console.WriteLine(string.Join(",", a));
+    }
+  }
+
+  public class Solution
+  {
+    public IList<IList<int>> Combine(int n, int k)
+    {
+      var result = new List<IList<int>>();
+      Backtrack(result, new List<int>(), n, k, 1);
+
+      return result;
+    }
+
+    private void Backtrack(List<IList<int>> result, List<int> temp, int n, int k, int start)
+    {
+      if (temp.Count == k)
+      { 
+        result.Add(new List<int>(temp));
+        return;
+      }
+
+      for (int i = start; i <= n; i++)
+      {
+        temp.Add(i);
+        Backtrack(result, temp, n, k, i + 1);
+        temp.RemoveAt(temp.Count - 1);
+      }
+    }
+  }
+}
